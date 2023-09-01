ed8f6f184d415c521c6ee5640851651c7b531d40
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2023-09-01 13:26:11 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc src/*.c vendor/sqlite3/*.c src/utils/*.c vendor/httplib/*.c src/methodFunction/*.c src/CashierFunction/*.c vendor/cJSON/cJSON.c vendor/tlse/tlse.c src/telegramClient/telegramClient.c vendor/str/str.c src/teleFunction/*.c -lpthread -o AplikasiKasir
+gcc src/*.c vendor/sqlite3/*.c src/utils/*.c vendor/httplib/*.c src/methodFunction/*.c src/CashierFunction/*.c vendor/cJSON/cJSON.c vendor/tlse/tlse.c src/telegramClient/telegramClient.c vendor/str/str.c src/teleFunction/*.c -lpthread -ldl -o AplikasiKasir
